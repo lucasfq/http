2e70f8343ba49982e50a5ae73db7184d6b4c50d1
lucasfq <75042830+lucasfq@users.noreply.github.com>
2020-12-03 14:12:23 -0300

    Update http_parser.dart

--- lib/src/http_parser.dart
+++ lib/src/http_parser.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/authentication_challenge.dart';
-export 'src/case_insensitive_map.dart';
-export 'src/chunked_coding.dart';
-export 'src/http_date.dart';
-export 'src/media_type.dart';
+export 'authentication_challenge.dart';
+export 'case_insensitive_map.dart';
+export 'chunked_coding.dart';
+export 'http_date.dart';
+export 'media_type.dart';
